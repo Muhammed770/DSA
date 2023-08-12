713a3c46ee7976220b5af2d9666cef4263b49bd6
Muhammed <b20cs51@gectcr.ac.in>
2023-08-12 16:01:52 +0530

    java dsa

---
+++ java/patterns/pattern2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class pattern2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+        for(int i=0; i< n; i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
