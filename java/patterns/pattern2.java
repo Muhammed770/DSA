7cce566f9eaed1815939d3ae07d12fa30642b6c0
Muhammed <b20cs51@gectcr.ac.in>
2023-08-12 16:50:14 +0530

    java dsa pattern2

--- java/patterns/pattern2.java
+++ java/patterns/pattern2.java
@@ -3,13 +3,25 @@ import java.util.Scanner;
 public class pattern2{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter a number");
-        int n = sc.nextInt();
-        for(int i=0; i< n; i++){
-            for(int j=0;j<=i;j++){
-                System.out.print("*");
+        // System.out.println("enter a number");
+        // int n = sc.nextInt();
+        int n =5;
+        int mid = n /2;
+        for(int i=0; i< n/2+1; i++){
+            for(int k=0; k< mid-i;k++ ){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i*2+1;j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
     }
 }
+
+
+
+
+//     * 
+//   * * * 
+// * * * * * 
