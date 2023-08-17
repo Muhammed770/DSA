bdfc7deaea309c143d253c3fe633c424c6242900
Muhammed <b20cs51@gectcr.ac.in>
2023-08-18 00:00:57 +0530

    printnto1

--- java/recursion/printNto1.java
+++ java/recursion/printNto1.java
@@ -7,13 +7,21 @@ public class printNto1 {
             int n = sc.nextInt();
             int[] result = printNos(n);
             sc.close();
-            System.out.println("result = " + result);
+            System.out.println("result = [");
+            for(int i=0;i<result.length;++i){
+                if(i>0){
+                    System.out.print(",");
+                }else {
+                    System.out.print(result[i]);
+                }
+            }
+            System.out.println("]");
 
     }
    public static int[] printNos(int x) {
         int[] arr = new int[0];
         int[] result = compute(x, arr);
-        return result;
+        return (int[])result;
     }
 
     public static int[] compute(int x,int[] arr) {
@@ -24,7 +32,7 @@ public class printNto1 {
             int[] newArray = Arrays.copyOf(arr,arr.length + 1);
             newArray[arr.length] = x;
             int[] result = compute(x-1, newArray);
-            return result;
+            return (int[]) result;
             
         }
     }
