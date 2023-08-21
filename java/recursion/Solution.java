b34b766ef8dca115960a7fb0dbe6d7ac45cab943
Muhammed <b20cs51@gectcr.ac.in>
2023-08-21 07:14:37 +0530

    factorialNumbers

---
+++ java/recursion/Solution.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Solution {
+    private static HashMap<Long, Long> memo = new HashMap<>();
+    
+    static {
+        memo.put(1L, 1L);
+    }
+
+    public static List<Long> factorialNumbers(Long n) {
+        List<Long> result = new ArrayList<>();
+        for (Long i = 1L; i < n; ++i) {
+            Long factorial = fact(i);
+            if (factorial <= n) {
+                result.add(factorial);
+            } else {
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static Long fact(Long n) {
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        } else {
+            Long factorial = n * fact(n - 1);
+            memo.put(n, factorial);
+            return factorial;
+        }
+    }
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        Long n = sc.nextLong();
+        List<Long> result = factorialNumbers(n);
+
+        for (Long num : result) {
+            System.out.print(num + " ");
+        }
+        sc.close();
+    }
+}
