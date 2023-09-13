f2cb4fedde6d8f39bb31e6d80eaf649f68ab0d18
Muhammed <b20cs51@gectcr.ac.in>
2023-09-13 23:47:47 +0530

    initial

--- algoexpert/hello.java
+++ algoexpert/hello.java
@@ -4,8 +4,10 @@ class Program {
   public static boolean isValidSubsequence(
     List<Integer> array, List<Integer> sequence
   ) {
+    int count = 0;
     for(int i=0; i<sequence.size(); ++i) {
       for(int j =count;j<sequence.size(); ++j)
+
     }
     return false;
   }
