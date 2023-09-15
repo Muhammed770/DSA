537612bbfdcb77ca8f9822a2ed244441107732bc
Muhammed <b20cs51@gectcr.ac.in>
2023-09-15 21:26:17 +0530

    i

---
+++ algoexpert/Program.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Program {
+    public static void main(String[] args) {
+        int i =5;
+        int[] array = {i};
+        System.out.print(array[0]);
+    }
+}
