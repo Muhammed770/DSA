964e9dec1e06d605652ec191c6976b135a147cde
Muhammed <b20cs51@gectcr.ac.in>
2023-09-16 23:16:01 +0530

    4.2

--- algoexpert/Prob4.java
+++ algoexpert/Prob4.java
@@ -29,8 +29,39 @@ class Prob4 {
       }
     }
 
-    
+
     return winner;
   }
 }
+
+// import java.util.*;
+
+// class Program {
+//   public String tournamentWinner(
+//     ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results
+//   ) {
+//     HashMap <String,Integer> hmap = new HashMap<>();
+//     String winningTeam="";
+//     for(int i=0;i<results.size();++i) {
+//        if(results.get(i)== 1) {
+//          winningTeam = competitions.get(i).get(0);
+//        } else {
+//          winningTeam = competitions.get(i).get(1);
+//        }
+//       hmap.put(winningTeam,hmap.getOrDefault(winningTeam,0)+3);
+//     }
+    
+//     String winner="";
+//     Integer maxScore=0;
+//     for(Map.Entry <String,Integer> entry: hmap.entrySet()) {
+//       if(maxScore<entry.getValue()) {
+//         maxScore = entry.getValue();
+//         winner = entry.getKey();
+//       }
+//     }
+//     //get max values key from hmap
+//     return winner;
+//   }
+// }
+
 
