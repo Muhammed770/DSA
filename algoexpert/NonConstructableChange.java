ce6d1f2d7871c8dd2364260cbcd0058f9bdf1002
Muhammed <b20cs51@gectcr.ac.in>
2023-09-16 23:57:58 +0530

    4.2

---
+++ algoexpert/NonConstructableChange.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class NonConstructibleChange {
+  public int nonConstructibleChange(int[] coins) {
+    // TreeSet<Integer> treeSet = new TreeSet<>();
+    Arrays.sort(coins);
+    int minChange=0;
+    for(int coin : coins) {
+      if(coin>minChange+1) {
+        return minChange+1;
+      } else {
+        minChange+=coin;
+      }
+    }
+    return minChange+1;
+  }
+}
