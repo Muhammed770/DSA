61076a5588f9c72cec0a456f2c4279d020d6fbcd
Muhammed <b20cs51@gectcr.ac.in>
2023-09-16 23:08:28 +0530

    4

---
+++ algoexpert/Prob4.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Prob4 {
+  public String tournamentWinner(
+    ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results
+  ) {
+    HashMap <String,Integer> hmap = new HashMap<>();
+    for(int i=0;i<results.size();++i) {
+      if(results.get(i) == 1) {
+        if(hmap.get(competitions.get(i).get(0)) == null) {
+          hmap.put(competitions.get(i).get(0),3);
+        } else {
+          hmap.put(competitions.get(i).get(0), hmap.get(competitions.get(i).get(0)) + 3);
+        }
+      }else if(results.get(i) == 0) {
+        if(hmap.get(competitions.get(i).get(1)) == null) {
+          hmap.put(competitions.get(i).get(1),3);
+        } else {
+          hmap.put(competitions.get(i).get(1), hmap.get(competitions.get(i).get(1)) + 3);
+        }
+      }
+    }
+    String winner="";
+    Integer maxScore=0;
+    for(Map.Entry <String,Integer> entry : hmap.entrySet()) {
+      if(entry.getValue()>maxScore) {
+        maxScore = entry.getValue();
+        winner = entry.getKey();
+      }
+    }
+    //get max values key from hmap
+    return winner;
+  }
+}
+
--- algoexpert/Program.java
+++ algoexpert/Program.java
@@ -3,7 +3,15 @@ import java.util.*;
 public class Program {
     public static void main(String[] args) {
         int i =5;
+        int temp=0;
+        int j = 0;
         int[] array = {i};
-        System.out.print(array[0]);
+        // for (; j < array.length; j++) {
+            
+        // }
+        // while (temp < 20) {
+        //     System.out.println("temp : "+ temp);
+        //     ++temp;
+        // }
     }
 }
