a15b9e77bddb304f6a6a3a41df250d4d6c19ac5a
Muhammed <b20cs51@gectcr.ac.in>
2023-09-21 06:37:46 +0530

    10.3

--- algoexpert/program.cpp
+++ algoexpert/program.cpp
@@ -31,9 +31,9 @@ int evaluateExpressionTree(BinaryTree *node)
         return l - r;
     case -3:
         if (r != 0)
-            //or could 
             return round(l / r);
         else
+            //or could throw eror
             return 0;
 
     case -4:
