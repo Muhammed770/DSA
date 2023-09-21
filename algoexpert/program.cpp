61aaebc743d42ea00d2a566d4b7cd0a355c3b6cc
Muhammed <b20cs51@gectcr.ac.in>
2023-09-21 06:35:54 +0530

    10.0

---
+++ algoexpert/program.cpp
@@ -0,0 +1,39 @@
+using namespace std;
+#include <cmath>
+// This is an input class. Do not edit.
+class BinaryTree
+{
+public:
+    int value;
+    BinaryTree *left = nullptr;
+    BinaryTree *right = nullptr;
+
+    BinaryTree(int value) { this->value = value; }
+};
+
+int evaluateExpressionTree(BinaryTree *node)
+{
+    if (node->left == nullptr && node->right == nullptr)
+    {
+        return node->value;
+    }
+    int l = evaluateExpressionTree(node->left);
+    int r = evaluateExpressionTree(node->right);
+    switch (node->value)
+    {
+    case -1:
+        return l + r;
+    case -2:
+        return l - r;
+    case -3:
+        if (r != 0)
+            return round(l / r);
+        else
+            return 0;
+
+    case -4:
+        return l * r;
+    default:
+        return 0;
+    }
+}
