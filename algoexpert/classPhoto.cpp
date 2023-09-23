82ab794955daf34f41d8433889f4248ffc38399c
Muhammed <b20cs51@gectcr.ac.in>
2023-09-23 11:58:07 +0530

    13.2

--- algoexpert/classPhoto.cpp
+++ algoexpert/classPhoto.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 bool classPhotos(vector<int> redShirtHeights, vector<int> blueShirtHeights)
 {
-
+    sort(redShirtHeights.begin(), redShirtHeights.end());
+    sort(blueShirtHeights.begin(), blueShirtHeights.end());
+    bool redTallest = true;
+    bool blueTallest = true;
     for (unsigned int i = 0; i < redShirtHeights.size(); ++i)
     {
-        if (redShirtHeights.at(i) >= blueShirtHeights.at(i) && redShirtHeights.at(0) <= blueShirtHeights.at(0))
+        if (redShirtHeights.at(i) <= blueShirtHeights.at(i))
         {
-            return false;
+            redTallest = false;
         }
-        else if (redShirtHeights.at(i) <= blueShirtHeights.at(i) && redShirtHeights.at(0) >= blueShirtHeights.at(0))
+        else if (blueShirtHeights.at(i) <= redShirtHeights.at(i))
         {
-            return false;
+            blueTallest = false;
         }
     }
-
-    return true;
+    return blueTallest || redTallest;
 }
