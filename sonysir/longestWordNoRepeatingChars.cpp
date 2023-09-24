95e4571ea719534c79033626f295e82ca3aa49c6
Muhammed <b20cs51@gectcr.ac.in>
2023-09-24 14:58:10 +0530

    16.2

--- sonysir/longestWordNoRepeatingChars.cpp
+++ sonysir/longestWordNoRepeatingChars.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
 #include<sstream>
 #include<vector>
+#include <unordered_set>
+
+bool hasRepeatingChars(const std::string& word) {
+    std::unordered_set<char> characters;
+    
+}
 
 int main() {
-        
+    std::string inputSentence;
+    std::cout<<"enter the sentence: ";
+    std::getline(std::cin,inputSentence);
+    std::istringstream iss(inputSentence);
+    // std::vector<std::string> words;
+    std::string word,longestWord="";
+    while(iss >> word) {
+        if(!hasRepeatingChars(word) && word.length() > longestWord.length()) {
+            longestWord = word;
+        }
+    }
+    std::cout<<"longest word :"<<longestWord;
+    return 0;
 }
