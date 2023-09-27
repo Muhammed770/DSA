3943db9ade47fec5c30257280f1968d768c76d8a
Muhammed <b20cs51@gectcr.ac.in>
2023-09-27 22:36:49 +0530

    17.1

--- sonysir/replaceRepeatingCharWithAsterisk.cpp
+++ sonysir/replaceRepeatingCharWithAsterisk.cpp
@@ -1,10 +1,32 @@
 #include<iostream>
 #include<sstream>
+#include<vector>
+
+std::string formatWord(std::string& word) {
+
+    std::string result = word;
+
+    for(int i=1 ;i<word.length();++i ) {
+        if(word[i-1] == word[i]) {
+            result[i-1] ='*';
+            result[i] = '*';
+        }
+    }
+    return result;
+}
 
 int main() {
     std::string inputString;
     std::getline(std::cin,inputString);
     std::istringstream iss(inputString);
     std::string word;
-    w
+    std::vector<std::string> sentence;
+    while(iss >> word){
+        std::string result = formatWord(word);
+        sentence.push_back(result);
+    }
+    for(unsigned int i=0;i< sentence.size();++i) {
+        std::cout<<sentence.at(i)<<" ";
+    }
+    
 }
