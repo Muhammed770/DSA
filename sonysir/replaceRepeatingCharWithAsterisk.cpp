e8482076c3dfc219a6683e532ce6b9d646a5a3bc
Muhammed <b20cs51@gectcr.ac.in>
2023-09-28 23:59:52 +0530

    17.35

--- sonysir/replaceRepeatingCharWithAsterisk.cpp
+++ sonysir/replaceRepeatingCharWithAsterisk.cpp
@@ -26,7 +26,7 @@ int main() {
         sentence.push_back(result);
     }
     for(unsigned int i=0;i< sentence.size();++i) {
-        std::cout<<sentence.at(i)<<"jkdnretk ";
+        std::cout<<sentence.at(i)<<"jkdfffnretk ";
     }
 
     
