373917f20e2ade6950844b188e953c4d591d3c1d
Muhammed <b20cs51@gectcr.ac.in>
2023-10-01 23:42:19 +0530

    17.36

--- sonysir/permutationRec.cpp
+++ sonysir/permutationRec.cpp
@@ -1,13 +1,20 @@
 #include <vector>
 using namespace std;
-void permutationHelper(vector<vector<int>> &ans,vector<int> array, vector<int> ds, int[] elements)
+void permutationHelper(vector<vector<int>> &ans,vector<int> array, vector<int> ds, vector<bool> used)
 {
+    if(ds.size()==array.size()) {
+        ans.push_back(ds);
+        return;
+    }
+    for(int i=0;i<array.size();++i) {
+        
+    }
 }
 vector<vector<int>> getPermutations(vector<int> array)
 {
     vector<vector<int>> ans;
     vector<int> ds;
-    int elements[array.size()] = {0};
-    permutationHelper(ans,array,ds,elements);
+    vector<bool> used(array.size(),false);
+    permutationHelper(ans,array,ds,used);
     return {};
 }
