576bc71c861b1ee0fbaf8c0137b8f3f61985c1af
Muhammed <b20cs51@gectcr.ac.in>
2023-10-02 06:51:24 +0530

    17.56

---
+++ sonysir/middleNode.cpp
@@ -0,0 +1,41 @@
+using namespace std;
+
+// This is an input struct. Do not edit.
+class LinkedList
+{
+public:
+    int value;
+    LinkedList *next = nullptr;
+
+    LinkedList(int value) { this->value = value; }
+};
+
+LinkedList *middleNode(LinkedList *linkedList)
+{
+    int count = 0;
+    LinkedList *current = linkedList;
+    while (current != nullptr) {
+        count++;
+        current = current->next;
+    }
+    if(count%2 ==0) {
+        int n = (count/2)+1;
+        LinkedList *current = linkedList;
+        while (current != nullptr && n!=0)
+        {
+            current = current->next;
+            n--;
+        }
+        return current;
+    }else {
+        int n = (count / 2) + 1;
+        LinkedList *current = linkedList;
+        while (current != nullptr && n != 0)
+        {
+            current = current->next;
+            n--;
+        }
+        return current;
+    }
+    return nullptr;
+}
