d6b9909163205e9ef6aeeb5e4de1d47801e8beca
Muhammed <b20cs51@gectcr.ac.in>
2023-10-03 15:35:18 +0530

    19

---
+++ sonysir/insertionsort.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+
+vector<int> insertionSort(vector<int> array)
+{
+    for (int i = 0;i<array.size();++i) {
+        int j=i;
+        while(j>0 && array[j]<array[j-1]) {
+            int temp=array[j];
+            array[j]=array[j-1];
+        }
+    }
+        return {};
+}
