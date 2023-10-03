5a02e1b898c9ac397d8246f89da7bebc1532afb4
Muhammed <b20cs51@gectcr.ac.in>
2023-10-03 15:36:50 +0530

    19.1

--- sonysir/insertionsort.cpp
+++ sonysir/insertionsort.cpp
@@ -8,7 +8,8 @@ vector<int> insertionSort(vector<int> array)
         while(j>0 && array[j]<array[j-1]) {
             int temp=array[j];
             array[j]=array[j-1];
+            array[j-1] = temp;
         }
     }
-        return {};
+        return array;
 }
