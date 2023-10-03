356b3e465f06ea61158e46bab3d5f92c9be1f265
Muhammed <b20cs51@gectcr.ac.in>
2023-10-03 17:05:04 +0530

    19.3

---
+++ sonysir/ceaser.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+string ceaserCypher(string str,int key) {
+    for(int i=0;i<str.length();++i) {
+        
+    }
+    return "";
+}
+int main() {
+    string str = "xyz";
+    int key=2;
+    string result = ceaserCypher(str,key);
+    return 0;
+}
