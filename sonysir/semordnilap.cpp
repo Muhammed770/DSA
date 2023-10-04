db9ccec94512347a68c9bae9124854869707e5d5
Muhammed <b20cs51@gectcr.ac.in>
2023-10-04 12:31:35 +0530

    22

--- sonysir/semordnilap.cpp
+++ sonysir/semordnilap.cpp
@@ -1,14 +1,32 @@
 #include <vector>
 #include <iostream>
 #include <sstream>
+#include <string>
+
+using namespace std;
 
 using namespace std;
 
 vector<vector<string>> semordnilap(vector<string> words)
 {
     vector<vector<string>> result;
-    for ()
-        return {};
+    int count = 0;
+    int i = 0;
+    for (string word : words)
+    {
+        string tempString = word;
+        reverse(word.begin(), word.end());
+        for (int j = i + 1; j < words.size(); ++j)
+        {
+            if (words[j] == word)
+            {
+                vector<string> pair = {words[j], tempString};
+                result.push_back(pair);
+            }
+        }
+        i++;
+    }
+    return result;
 }
 
 int main()
@@ -16,9 +34,11 @@ int main()
     string inputString;
     getline(cin,inputString);
     istringstream iss(inputString);
-    string str;
-    while (iss >> str) {
-        cout<<str<< "***";
+    string word;
+    vector<string> strinds;
+    while (iss >> word)
+    {
+        strings.push_back(word);
     }
     // string str = "AAAAAAAAAAAAAaaaAAAAaaaBBBCCCDDDD";
     int key = 2;
