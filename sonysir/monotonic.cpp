569c6bd47ec54becf4e1dbef2178b24bd4d53cfe
Muhammed <b20cs51@gectcr.ac.in>
2023-10-06 12:04:30 +0530

    25.3

--- sonysir/monotonic.cpp
+++ sonysir/monotonic.cpp
@@ -5,9 +5,24 @@ using namespace std;
 bool isMonotonic(vector<int> array)
 {
     int n = array.size();
-    int arrayNature = array[0] - array[n - 1];
-    if (arrayNature == 0) {
-        return false;
+    if(n<=1) {
+        return true;
     }
-    return false;
+    // int arrayNature = array[0] - array[n - 1];
+    int flag=0;
+    for(int i=1;i<n;++i) {
+        if(array[i-1]<array[i] && flag!=-1) {
+            flag=1;
+        }
+        else{
+            return false;
+        }
+        if (array[i - 1] > array[i] && flag != 1) {
+            flag=-1;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
 }
