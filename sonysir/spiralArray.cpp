9c88cfadfcb5d77f5de0cf9dfcbf16eb4cd26a5c
Muhammed <b20cs51@gectcr.ac.in>
2023-10-11 21:18:16 +0530

    cs45

--- sonysir/spiralArray.cpp
+++ sonysir/spiralArray.cpp
@@ -1,17 +1,27 @@
 #include <vector>
 using namespace std;
 
+void getTransPose(vector<vector<int>> array) {
+    int m = array.size();
+    int n = array[0].size();
+    vector<vector<int>> transpose(n , vector<int>(m,0));
+    for(int i=0;i<m;++i) {
+        for(int j=0;j<n;++j) {
+            transpose[j][i] = array[i][j];
+        }
+    }
+    array = transpose;
+}
+ 
 vector<int> spiralTraverse(vector<vector<int>> array)
 {
     int m = array.size();
     int n = array[0].size();
     vector<int> result(m * n);
-    int rowMin = 0;
-    int colMin = 0;
-    int rowMax = m;
-    int colMax = n;
-    int dir=1;
-    while(rowMin<=rowMax && colMin<=colMax) {
+    while(array.size() > 0) {
+        result.push_back(array[0]);
+        array.erase()
+        getTransPose(array);
         
     }
     return {};
